c6aa714ef3d187313e3c8f6392f9b160e5e420b5
Iwona Sztorc <ivsztorc@gmail.com>
2016-01-14 22:24:41 +0000

    set up login/sign up functionality, added background image, set up home page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114141124) do
+ActiveRecord::Schema.define(version: 20160114222301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160114141124) do
     t.string   "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
   create_table "usertrips", force: :cascade do |t|
