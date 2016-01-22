2d89362e4513926215d3b66bbbd0c052e7fd7a04
Iwona Sztorc <ivsztorc@gmail.com>
2016-01-22 19:44:05 +0000

    added a friendship functionality but not working properly yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
   has_many :expenses
+  # has_many :friendships
+  # has_many :friends, :through => :friendships
+  # has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  # has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+
+  has_many :friends, :through => :friendships
+  has_many :requested_friends, :through => :friendships, :source => :friend
+  has_many :pending_friends, :through => :friendships, :source => :friend
+  has_many :friendships, :dependent => :destroy
+
+
   # has_many :feedbacks, :through => :passenger
 
   before_create :set_default_role
@@ -20,25 +31,25 @@ class User < ActiveRecord::Base
   def role?(role_to_compare_to)
     role_to_compare_to.to_s == self.role.to_s
   end
-end
 
-def set_default_role
-  self.role = "user" unless role
-end 
+  def set_default_role
+    self.role = "user" unless role
+  end 
 
-def role (role_to_compare)
-  self.role.to_s == role_to_compare.to_s
-end
+  # def role (role_to_compare)
+  #   self.role.to_s == role_to_compare.to_s
+  # end
 
-def mailboxer_name
-  self.name
-end
+  def mailboxer_name
+    self.name
+  end
 
-def mailboxer_email(object)
+  def mailboxer_email(object)
 
- if object.class==Mailboxer::Notification
-   return nil
- else
-   email
- end
+   if object.class==Mailboxer::Notification
+     return nil
+   else
+     email
+   end
+  end
 end
---
+++ db/migrate/20160122114306_create_friendships.rb
@@ -0,0 +1,12 @@
+class CreateFriendships
+def self.up
+create_table :friendships do |t|
+t.integer :user_id, :friend_id
+t.string :status
+t.timestamps
+end
+end
+def self.down
+drop_table :friendships
+end
+end
