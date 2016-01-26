d5671605e07351f1327782fdebdc477ff6dd9bfe
Iwona Sztorc <ivsztorc@gmail.com>
2016-01-26 17:09:31 +0000

    fixed the expense id

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -62,6 +62,6 @@ class TripsController < ApplicationController
       end
       
       def trip_params
-        params.require(:trip).permit(:tripname, :country_id, :currency_id, :participant_id)
+        params.require(:trip).permit(:tripname, :country_id, :currency_id, :participant_id, :expense_id)
       end
 end
