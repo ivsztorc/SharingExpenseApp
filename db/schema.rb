a354c332c4b33b6ebefb152084c9e1c968776000
Iwona Sztorc <ivsztorc@gmail.com>
2016-01-27 17:43:08 +0000

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126164033) do
+ActiveRecord::Schema.define(version: 20160127172057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,13 +38,6 @@ ActiveRecord::Schema.define(version: 20160126164033) do
     t.text     "expense_image"
     t.datetime "datepicker"
     t.integer  "trip_id"
-  end
-
-  create_table "friendships", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "friend_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "participants", force: :cascade do |t|
